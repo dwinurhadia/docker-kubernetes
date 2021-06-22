cc67e5f5e43c37119d6fd45a06ac1eead985d641
Dwi Nurhadi Afriyanto <dwi.nurhadi17@student.uns.ac.id>
2021-06-21 21:56:42 -0400

    add index js

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ var app = express();
 
 app.get('/', function(req,res)
 {
-    res.send("Hello World! v3");
+    res.send("Hi, Welcome to Express);
 });
 
 var server = app.listen(3000, function(){
     var host = server.address().address;
     var port = server.address().port;
-
     console.log('Example app listining at http://%s:%s',host,port);
 });
